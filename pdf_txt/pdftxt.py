d0605c65c321050ecbe28b8609e9e2a7b34727bf
vartika-arora <vartikaarora2@gmail.com>
2017-11-12 19:28:37 +0530

    added basics

---
+++ pdf_txt/pdftxt.py
@@ -0,0 +1,21 @@
+import PyPDF2
+import textract
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+filename = 'COUNTRY MATRIX.pdf'
+pdfFileObj = open(filename,'rb')
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+num_pages = pdfReader.numPages
+count = 0
+text = ""
+while count < num_pages:
+    pageObj = pdfReader.getPage(count)
+    count +=1
+    text += pageObj.extractText()
+if text != "":
+   text = text
+   text = textract.process(fileurl, method='tesseract', language='eng')
+tokens = word_tokenize(text)
+punctuations = ['(',')',';',':','[',']',',']
+stop_words = stopwords.words('english')
+keywords = [word for word in tokens if not word in stop_words and  not word in string.punctuation]
